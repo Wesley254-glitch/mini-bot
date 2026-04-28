199397e6398c5e2f382b15e073bfad24f5cbb918
Wesley254-glitch <w3214142@gmail.com>
2026-04-28 10:59:29 +0300

    Initialize WhatsApp client with QR and message handling

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
+    }
+});
+
+client.on('qr', async (qr) => {
+    console.log('QR received, requesting pairing code...');
+    const pairingCode = await client.requestPairingCode('254738550932');
+    console.log('PAIRING CODE:', pairingCode);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body === '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
